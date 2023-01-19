dfea0c2252fa2508f27b60393f7efb9eee2feb97
Tommy Tran <haitran1995@gmail.com>
2023-01-18 23:49:50 -0500

    Add new function example

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
-const help = (items) => {
-  return items.filter((value, index) => {
-    return items.indexOf(value) === index;
-  });
-};
+// const help = (items) => {
+//   return items.filter((value, index) => {
+//     return items.indexOf(value) === index;
+//   });
+// };
 
-const items = ["cheese", "cheese", "pizza", "hotdog", "pizza"];
+// const items = ["cheese", "cheese", "pizza", "hotdog", "pizza"];
 
-console.log(help(items));
+// console.log(help(items));
+let num = 1;
+
+console.log(num);
+let num = 2;
