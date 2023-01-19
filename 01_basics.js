06c19ebfd8169cd553e73c80357286e3508a2672
Tommy Tran <haitran1995@gmail.com>
2023-01-18 23:59:27 -0500

    compiled ts to make basics js

---
+++ 01_basics.js
@@ -0,0 +1,5 @@
+// username can only be changed into the type 'string'
+var username = 'Tommy';
+username = 'Bob';
+username = 42;
+// username = false;
---
+++ 01_basics.ts
@@ -0,0 +1,7 @@
+// username can be changed into string OR number
+let username: string | number = 'Tommy';
+
+username = 'Bob';
+username = 42;
+
+// if compiled and nothing shows up, its a good thing.
