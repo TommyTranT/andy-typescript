11495ec5f606768196b9b9d007dcff1bd6637083
Tommy Tran <haitran1995@gmail.com>
2023-01-20 23:01:34 -0500

    inline object type

---
+++ 02_objects.ts
@@ -0,0 +1,8 @@
+// creating a brand new 'type' and an object that fullfills that type
+const user: {
+  username: string;
+  password: string;
+} = {
+  username: 'tommytran100',
+  password: '1234'
+};
