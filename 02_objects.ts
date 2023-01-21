cbeb6faf2185202480588130cc0f604ce902b764
Tommy Tran <haitran1995@gmail.com>
2023-01-20 23:05:24 -0500

    add interface for username

--- 02_objects.ts
+++ 02_objects.ts
@@ -1,8 +1,21 @@
 // creating a brand new 'type' and an object that fullfills that type
-const user: {
+const userA: {
   username: string;
   password: string;
 } = {
   username: 'tommytran100',
   password: '1234'
 };
+
+// However, this is not the optimal way to do it and can be hard to read.
+// To create your own type, you should create an "Interface" that can be used like "string" or "number".
+
+interface User {
+  username: string;
+  password: string;
+}
+
+const userB: User = {
+  username: 'tommytran100',
+  password: '1234'
+// }
