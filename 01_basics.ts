269899840e881b0182409f6eb209fd39e9cfcb84
Tommy Tran <haitran1995@gmail.com>
2023-01-20 22:57:46 -0500

    dont need to type the type all the time

--- 01_basics.ts
+++ 01_basics.ts
@@ -15,4 +15,7 @@ const nums: (string | number)[] = [];
 nums.push(4);
 nums.push('hello')
 
+// dont always have to type the type if its declared already. 
+const myNum = nums.pop();
+
 // if compiled and nothing shows up, its a good thing.
