c4cb6107ae5e9130dfd062ecd3c3b716a6ac9089
Tommy Tran <haitran1995@gmail.com>
2023-01-24 15:32:33 -0500

    add ts function

---
+++ 03_functions.ts
@@ -0,0 +1,7 @@
+// what do you need to consume a function?
+// arguments, return value, name
+
+// function argument is string. it will return a string. the string returned is in the return
+const sayHello = (name: string): string => {
+  return `hello there ${name}`
+}
